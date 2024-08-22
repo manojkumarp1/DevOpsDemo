0c29ee97fc02a8ef9f114bfa50568a14e55a3f00
KesavaChari-V <chari21761a0560@gmail.com>
2024-08-22 10:45:39 +0530

    Update Demo.java

--- DemoProject/src/com/lbrce/demo/Demo.java
+++ DemoProject/src/com/lbrce/demo/Demo.java
@@ -5,6 +5,7 @@ public class Demo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         System.out.println("Devops");
+		System.out.println("MG Manoj");
 	}
 
 }
