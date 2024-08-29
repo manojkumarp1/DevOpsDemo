d9698990c32ba6586a2f1c53c517ee7d830cd28c
AdmiN <AdmiN@ITWS0047>
2024-08-29 09:41:45 +0530

    commit01

--- DemoProject/src/com/lbrce/demo/Demo.java
+++ DemoProject/src/com/lbrce/demo/Demo.java
@@ -5,7 +5,13 @@ public class Demo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         System.out.println("Devops");
-		System.out.println("MG Manoj");
+		System.out.println("Manoj");
+	} 
+	
+public class Demo2 {
+	public static void main(String[] args) {
+		System.out.println("Hello");
 	}
+}
 
 }
